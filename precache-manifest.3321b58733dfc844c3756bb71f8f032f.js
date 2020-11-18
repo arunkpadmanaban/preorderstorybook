5d5992a582505c25eebd81c0ab352dc3ab3cdfee
arunkpadmanaban <31412150+arunkpadmanaban@users.noreply.github.com>
2020-11-18 15:08:00 -0600

    Add files via upload

---
+++ precache-manifest.3321b58733dfc844c3756bb71f8f032f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d38c9f9f6606fa0dac9357cffabe53",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.324d9aeadb2bc04dd6db.bundle.js"
+  },
+  {
+    "url": "runtime~main.324d9aeadb2bc04dd6db.bundle.js"
+  },
+  {
+    "revision": "eb6891146640deabb5256d3125fcdd1b",
+    "url": "static/media/UniversforBP-Bold.eb689114.ttf"
+  },
+  {
+    "revision": "5c1aa6abe7d1f0e41cbf05330a1f140c",
+    "url": "static/media/UniversforBP-Light.5c1aa6ab.ttf"
+  },
+  {
+    "revision": "c26a0fcf95e302a660bd9e7115742226",
+    "url": "static/media/UniversforBP-Roman.c26a0fcf.ttf"
+  },
+  {
+    "url": "vendors~main.324d9aeadb2bc04dd6db.bundle.js"
+  },
+  {
+    "url": "vendors~main.324d9aeadb2bc04dd6db.bundle.js.LICENSE.txt"
+  }
+]);
